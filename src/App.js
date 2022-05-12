b26e50813e14457184be6c48c54dcd84ff080d0d
Aneer Fidalgo <aneer@Aneers-MacBook-Air.local>
2022-05-12 16:58:00 -0400

    my first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [firstName, setFirstName] = useState("*");
+  const [isLoggedIn, setIsLoggedin] = useState(false);
+  const loginlogout = () => setIsLoggedin(!isLoggedIn);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>You clicked the button {count} times.</h1>
+        <button onClick={() => setCount(count + 1)}> + </button>&nbsp;
+        <button onClick={() => setCount(0)}> Reset</button>&nbsp;
+        <button onClick={() => setCount(count ? count - 1 : 0)}> - </button>
+        &nbsp;
+        <button onClick={() => setCount(Math.max)(count - 1, 0)}> - </button>
+        <h2>Welcome Guest</h2>
+        <button onClick={loginlogout}>{isLoggedIn ? "Logout" : "login"}</button>
+        <section>
+          <h2 onClick={() => setFirstName(firstName + "*")}>
+            Hello {firstName}
+          </h2>
+        </section>
       </header>
     </div>
   );
 }
-
 export default App;
